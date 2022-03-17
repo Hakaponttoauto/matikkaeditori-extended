88511e025cb9f20fed798cfd53785c86dd335ff8
Hakaponttoauto <petri.heinamaki@iki.fi>
2022-03-17 22:06:08 +0200

    fix HTML injections

--- src/util.js
+++ src/util.js
@@ -15,44 +15,53 @@ function recurseFind(obj, test) {
     return undefined;
 }
 
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;")
+}
+
 function toJson(child) {
 	if (child.nodeName == "IMG" && child.style.display != "none") {
-             	if (child.getAttribute("src").startsWith("/math.svg") && child.alt.length > 0) {
-                    return {math: child.alt}
-                } else {
-                    return {pic: child.getAttribute("src")}
-                }
-            } else if (child.nodeName == "#text") {
-                return {text: child.textContent}
-            } else if (child.nodeName == "BR") {
-                return {}
-            }
+        if (child.getAttribute("src").startsWith("/math.svg") && child.alt.length > 0) {
+            return {math: child.alt}
+        } else {
+            return {pic: child.getAttribute("src")}
+        }
+    } else if (child.nodeName == "#text") {
+        return {text: child.textContent}
+    } else if (child.nodeName == "BR") {
+        return {}
+    }
 }
 
 function fromJson(child) {
-		if (child.math) {
-	                return `<img alt="${child.math}" src="/math.svg?latex=${encodeURIComponent(child.math)}">`
-	            } else if (child.pic) {
-	                return `<img src="${child.pic}">`
-	            } else if (child.text) {
-	                return child.text
-	            } else {
-	          	return "<br>"
-            	    }
+    if (child.math) {
+        return `<img alt="${escapeHtml(child.math)}" src="/math.svg?latex=${encodeURIComponent(child.math)}">`
+    } else if (child.pic) {
+        return `<img src="${child.pic}">`
+    } else if (child.text) {
+        return escapeHtml(child.text)
+    } else {
+        return "<br>"
+    }
 }
 
 function getAnswer() {
     let res = []
     for (element of document.getElementById("answer1").childNodes) {
-	if (element.nodeName == "DIV") {
-        	let group = []
-        	for (child of element.childNodes) {
-            		group.push(toJson(child))
-        	}
-        	res.push(group)
-	} else {
-		res.push(toJson(element))
-	}
+        if (element.nodeName == "DIV") {
+            let group = []
+            for (child of element.childNodes) {
+                group.push(toJson(child))
+            }
+            res.push(group)
+        } else {
+            res.push(toJson(element))
+        }
     }
     return res
 }
@@ -60,15 +69,15 @@ function getAnswer() {
 function setAnswer(answer) {
     let res = ""
     for (group of answer) {
-	if (Array.isArray(group)) {
-        	res += "<div>"
-	        for (child of group) {
-	            res += fromJson(child)
-        	}
-        	res += "</div>"
-	} else {
-		res += fromJson(group)
-	}
+        if (Array.isArray(group)) {
+            res += "<div>"
+            for (child of group) {
+                res += fromJson(child)
+            }
+            res += "</div>"
+        } else {
+            res += fromJson(group)
+        }
     }
     document.getElementById("answer1").innerHTML = res
 }
